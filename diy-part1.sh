f80827949b16ad9dd0ee30048d3b74395e27e141
hongcz1104 <83286089+hongcz1104@users.noreply.github.com>
2021-12-21 15:17:15 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# 移除原版软件包    
+rm -rf ./package/lean/aliyundrive-webdav
+rm -rf ./package/lean/luci-app-aliyundrive-webdav
+rm -rf ./package/lean/go-aliyundrive-webdav
+rm -rf ./package/lean/luci-app-go-aliyundrive-webdav
+rm -rf ./package/lean/luci-app-cpufreq
+  
+  
+# 添加自己的插件库
+echo -e "\nsrc-git hongcz https://github.com/hongcz1104/openwrt-packages" >> feeds.conf.default
+#echo -e "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+#echo -e "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+#echo -e "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter" >> feeds.conf.default
