2d068067e37ff973a87caef92766480fa8c49fb7
hongcz1104 <83286089+hongcz1104@users.noreply.github.com>
2021-12-26 16:27:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,6 +8,7 @@ rm -rf ./package/lean/luci-app-aliyundrive-webdav
 rm -rf ./package/lean/luci-app-cpufreq
 rm -rf ./package/lean/luci-app-kodexplorer
   
-# 添加自己的插件库
-echo -e "\nsrc-git hongcz https://github.com/hongcz1104/openwrt-packages" >> feeds.conf.default
+# 添加插件库
+echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
+svn checkout  https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash ./package/lean/luci-app-openclash
 #echo -e "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter" >> feeds.conf.default
