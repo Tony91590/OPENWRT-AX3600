fd47b3f7d47b1c4b2d218b33ae5be0c8ba111468
hongcz1104 <83286089+hongcz1104@users.noreply.github.com>
2021-12-27 00:27:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,7 +24,7 @@ sed -i '/exit/i\uci commit network' package/lean/default-settings/files/zzz-defa
 # 替换私人设置
 curl -fsSL https://raw.githubusercontent.com/hongcz1104/OPENWRT-AX3600/config/mac80211.sh > package/kernel/mac80211/files/lib/wifi/mac80211.sh
 curl -fsSL https://raw.githubusercontent.com/hongcz1104/OPENWRT-AX3600/config/ddnsto.config > feeds/xiangfeidexiaohuo/nas/ddnsto/files/ddnsto.config
-#curl -fsSL https://raw.githubusercontent.com/hongcz1104/OPENWRT-AX3600/config/openclash > feeds/xiangfeidexiaohuo/nas/ddnsto/files/ddnsto.config
+curl -fsSL https://raw.githubusercontent.com/hongcz1104/OPENWRT-AX3600/config/openclash > package/lean/luci-app-openclash/root/etc/config/openclash
 
 # 取消bootstrap为默认主题，改为argon-18.06
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
