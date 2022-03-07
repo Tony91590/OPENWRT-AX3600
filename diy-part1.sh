317b2188dd7ddf4d77a8d6d1cb8c4adae359f75a
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2022-03-07 22:09:44 +0100

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,23 +1,19 @@
-##添加自己的插件库
-rm -rf ./package/lean/k3screenctrl
-rm -rf ./package/lean/luci-app-syncdial
-rm -rf ./package/lean/luci-theme-argon
-rm -rf ./package/lean/luci-app-jd-dailybonus
-rm -rf ./package/lean/luci-lib-docker
-rm -rf ./package/lean/luci-app-pushbot
-rm -rf ./package/lean/luci-app-netdata
-rm -rf ./package/lean/aliyundrive-webdav
-rm -rf ./package/lean/luci-app-aliyundrive-webdav
-rm -rf ./package/lean/go-aliyundrive-webdav
-rm -rf ./package/lean/luci-app-go-aliyundrive-webdav
-rm -rf ./package/lean/luci-app-easymesh
-rm -rf ./package/lean/luci-app-cpufreq
-rm -rf ./package/lean/luci-app-kodexplorer
-  
-# 添加插件库
-echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
-#echo -e "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
-#echo -e "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-#echo -e "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter" >> feeds.conf.default
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-svn checkout  https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash ./package/lean/luci-app-openclash
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+
+#echo 'src-git Boos https://github.com/Boos4721/OpenWrt-Packages.git' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
